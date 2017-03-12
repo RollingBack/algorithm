4c92c07ad18739bfc4667a13643c60b9f70df103
RollingBack <lackgod@hotmail.com>
2017-03-12 09:21:33 +0800

    修复BUG

--- euler/euler.py
+++ euler/euler.py
@@ -7,6 +7,8 @@
 # @Software: PyCharm
 
 def is_prime(x):
+    if x == 1:
+        return False
     for i in range(2, int(x/2)+1):
         if x%i == 0:
             return False
--- euler/truncatable_primes.py
+++ euler/truncatable_primes.py
@@ -5,4 +5,28 @@
 # @Email    : lackgod@hotmail.com
 # @File    : truncatable_primes.py
 # @Software: PyCharm
+from euler import is_prime
+from itertools import count
+import re
+
+def is_trunc(x):
+        y = str(x)
+        for i in range(1, len(y)):
+            if not is_prime(int(y[i:])) or not is_prime(int(y[:-i])):
+                return False
+        return True
+
+total = 0
+sum1 = 0
+for each in count(11):
+    if not (each > 100 and re.search('[245680]', str(each))):
+        # print(each)
+        if is_prime(each) and is_trunc(each):
+            total += 1
+            sum1 += each
+            if total == 11:
+                break
+print(sum1)
+
+
 
